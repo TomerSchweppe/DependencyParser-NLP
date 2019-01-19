e6a4f52ee159413598a5939cd2832749f2d218f4
tomer <schweppetomer@gmail.com>
2019-01-19 11:36:14 +0200

    improved memory consumption & bug fix

--- DT/Code_Directory/perceptron.py
+++ DT/Code_Directory/perceptron.py
@@ -1,8 +1,6 @@
 # !/usr/bin/env python
 from chu_liu import *
 import numpy as np
-import time
-from random import randint
 from random import shuffle
 
 class Perceptron:
@@ -13,57 +11,32 @@ class Perceptron:
         self._features = features
         self._f_dict_list = self.extract_features(data, features)
         self._full_graphs = dict()
+        self._window_list = self.window_list()
         for sentence in self._data.sentences:
             if sentence.sentence_len not in self._full_graphs:
                 self._full_graphs[sentence.sentence_len] = self.full_graph(sentence.sentence_len)
 
-    def prune(self, h, m, sentence):
-        """prune implausible edges"""
-        # source: http://aclweb.org/anthology/C10-1007
-        h_pos = sentence(h)[1]
-        m_pos = sentence(m)[1]
+    def window_list(self):
+        """save window list"""
+        win_list = []
+        for _, window in self._features(0, 1, self._data.sentences[0]):
+            win_list.append(window)
+        return win_list
 
-        if h_pos in ['"', '.', ';', '|', 'CC', 'PRP$', 'PRP', 'EX', '-RRB-', '-LRB-']: return True
-
-        if m_pos in ['EX', 'LS', 'POS', 'PRP$'] and h < m: return True
-
-        if m_pos in ['.', 'RP'] and h > m: return True
-
-        if h == 0 and h_pos in [',', 'DT']: return True
-
-        if h < m:
-            if h_pos == 'DT' and m_pos in ['DT', 'JJ', 'NN', 'NNP', 'NNS', '.']: return True
-
-            if h_pos == 'CD' and m_pos == 'CD': return True
-
-            if h_pos == 'NN' and m_pos in ['DT', 'NNP']: return True
-
-            if h_pos == 'NNP' and m_pos in ['DT', 'NN', 'NNS']: return True
-
-        if h > m:
-            if m_pos == 'DT' and h_pos in ['DT', 'IN', 'JJ', 'NN', 'NNP']: return True
-
-            if m_pos == 'NNP' and h_pos == 'IN': return True
-
-            if m_pos == 'IN' and h_pos == 'JJ': return True
-        return False
-
-    def features_dict(self, sentence, features):
+    def features_dict(self, sentence):
         """generate features dictionary per sentence"""
         f_dict = dict()
         for h in range(sentence.sentence_len):
             for m in range(1, sentence.sentence_len):
-                if  not self.prune(h, m ,sentence):
-                    f_dict[(h, m)] = features(h, m, sentence)
-                else:
-                    f_dict[(h, m)] = []
+                if h != m:
+                    f_dict[(h, m)] = [shift for shift, _ in self._features(h, m, sentence)]
         return f_dict
 
     def extract_features(self, data, features):
         """extract features for all sentences"""
         f_dict_list = []
         for sentence in data.sentences:
-            f_dict_list.append(self.features_dict(sentence, features))
+            f_dict_list.append(self.features_dict(sentence))
         return f_dict_list
 
     def sentence_inference(self, w, sentence_len, f_dict):
@@ -71,7 +44,7 @@ class Perceptron:
         def get_score(h, m):
             pos = 0
             score = 0
-            for shift, window in f_dict[(h, m)]:
+            for shift, window in zip(f_dict.get((h, m), -1), self._window_list):
                 if shift != -1:
                     score += w[pos + shift]
                 pos += window
@@ -90,21 +63,39 @@ class Perceptron:
             g[h] = [m for m in range(1, node_num) if m != h]
         return g
 
-    def update_weights(self, w, d_tree, sentence, f_dict, sign):
+    def update_weights(self, w, exact_d_tree, infer_d_tree, f_dict):
         """update weights"""
-        for h, m_list in d_tree.items():
+        for h, m_list in exact_d_tree.items():
             for m in m_list:
-                pos = 0
-                for shift, window in f_dict[(h, m)]:
-                    if shift != -1:
-                        w[pos + shift] += sign
-                    pos += window
+                if m not in infer_d_tree.get(h, []):
+                    pos = 0
+                    for shift, window in zip(f_dict.get((h, m), -1),  self._window_list):
+                        if shift != -1:
+                            w[pos + shift] += 1
+                        pos += window
+
+        for h, m_list in infer_d_tree.items():
+            for m in m_list:
+                if m not in exact_d_tree.get(h, []):
+                    pos = 0
+                    for shift, window in zip(f_dict.get((h, m), -1), self._window_list):
+                        if shift != -1:
+                            w[pos + shift] += -1
+                        pos += window
+
+
+    def compare_trees(self, tree1, tree2):
+        """compare trees"""
+        for h, m_list in tree1.items():
+            for m in m_list:
+                if m not in tree2.get(h, []):
+                    return False
+        return True
 
 
     def train(self, N):
         """
         train the model
-
         :param N: number of iterations
         :return w: learnt weights
         """
@@ -115,15 +106,11 @@ class Perceptron:
             for idx in indices:
                 sentence = self._data.sentences[idx]
                 inference_d_tree = self.sentence_inference(w, sentence.sentence_len, self._f_dict_list[idx])
-                if inference_d_tree != sentence.dependency_tree():
-                    self.update_weights(w, sentence.dependency_tree(), sentence, self._f_dict_list[idx], 1)
-                    self.update_weights(w, inference_d_tree, sentence, self._f_dict_list[idx], -1)
+                if not self.compare_trees(sentence.dependency_tree(), inference_d_tree):
+                    self.update_weights(w, sentence.dependency_tree(), inference_d_tree, self._f_dict_list[idx])
             shuffle(indices)
         return w
 
 
 if __name__ == '__main__':
     print('PASSED!')
-
-
-
